1bf34a7e4d9ecd8df0a1d22b9777420602ac8666
PrabhavGupta24 <guptaprabhav24@gmail.com>
2025-03-29 12:06:28 -0400

    reordered the points table after removing elements

--- qualification_scenarios/remove_matches.py
+++ qualification_scenarios/remove_matches.py
@@ -1,5 +1,6 @@
 import csv
 from scrape_curr_data import export_data
+from collections import OrderedDict
 
 def extract_data(pt_filepath, schedule_filepath):
     pt_fieldnames = []
@@ -34,7 +35,7 @@ def remove_matches(
             pt_data[team1]['Matches'] = int(pt_data[team1]['Matches']) - 1
             pt_data[team2]["Matches"] = int(pt_data[team2]["Matches"]) - 1
 
-            if match['Tied/NR']:
+            if int(match['Tied/NR']):
                 pt_data[team1]["Tied/NR"] = int(pt_data[team1]["Tied/NR"]) - 1
                 pt_data[team2]["Tied/NR"] = int(pt_data[team2]["Tied/NR"]) - 1
 
@@ -50,6 +51,9 @@ def remove_matches(
 
         schedule_data[i] = {k: v for k, v in match.items() if k in s_fieldname_keep}
 
+    print(pt_data.keys())
+    pt_data = OrderedDict(sorted(pt_data.items(), key=lambda item: item[1]['Points'], reverse=True))
+    print(pt_data.keys())
 
     return pt_data, schedule_data
 
@@ -65,6 +69,7 @@ def main():
     new_schedule_filepath = "data/ipl_2024_schedule_edit.csv"
     pt_data, schedule_data = remove_matches(20, pt_data, schedule_data)
     export_data(new_pt_filepath, pt_fieldnames, pt_data)
+    #make ordered dict so that the points table comes out in order
 
     schedule_data = {entry['Match Number']: entry for entry in schedule_data}
     export_data(new_schedule_filepath, schedule_fieldnames, schedule_data)

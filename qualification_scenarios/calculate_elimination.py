27dfdf30e8daeb649d4e2a0aa1405683dd212ed1
PrabhavGupta24 <guptaprabhav24@gmail.com>
2025-03-29 22:16:32 -0400

    fixed negative edge weights bug

--- qualification_scenarios/calculate_elimination.py
+++ qualification_scenarios/calculate_elimination.py
@@ -2,7 +2,7 @@ import networkx as nx
 from itertools import combinations
 import csv
 import time
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 import math
 
 def get_schedule_data(filepath):
@@ -18,7 +18,7 @@ def get_schedule_data(filepath):
     return matches, total_matches
 
 def get_points_table_data(filepath):
-    teams = {}
+    teams = OrderedDict()
     with open(filepath, mode="r", newline="") as file:
         reader = csv.DictReader(file)
         for team in reader:
@@ -44,7 +44,7 @@ def create_graph(teams, matches, using_points):
 
     return G, source, sink
 
-def get_possibility(target_team, pt_filepath, schedule_filepath, allow_ties):
+def get_possibility(target_team, pt_filepath, schedule_filepath, allow_ties, top_n=1):
 
     # Get teams and their points, remove target team
     teams_data = get_points_table_data(pt_filepath)
@@ -66,32 +66,28 @@ def get_possibility(target_team, pt_filepath, schedule_filepath, allow_ties):
         target_team_points + 2 * target_team_games_left 
         if allow_ties 
         else math.floor(target_team_points / 2) + target_team_games_left
-    ) 
+    )
+
+    # Set number of wins that each team can still get
     for team in teams_data:
         teams_data[team] = (
             target_team_max - teams_data[team]
             if allow_ties
             else target_team_max - math.ceil(teams_data[team] / 2)
         )
-        if teams_data[team] < 0:
-            print(f"Simple Check Failed, {target_team} cannot as many points as {team}.")
-            return (
-                False, None, None, None,None
-            )
 
+    # Make the graph
     G, source, sink = create_graph(teams_data, matches, allow_ties)
 
-    print(G)
-    # for i in G.edges(data=True):
-    #     print(i)
+    # Get parameters for calculating max flow, and calculate 
+    target_max_flow = ((int(allow_ties)) + 1) * games_to_schedule
+    teams = set(all_teams) - {target_team}
 
-    max_flow_val, max_flow_path = nx.maximum_flow(G, source, sink)
-    # print(max_flow_val)
-    # print(max_flow_val + target_team_games_left)
-    # print(max_flow_path)
+    success, max_flow_val, max_flow_path = get_max_flow(G, source, sink, teams, teams_data, target_max_flow, top_n)
+
+
     return (
-        True,
-        max_flow_val == ((int(allow_ties)) + 1) * games_to_schedule,
+        success,
         (
             math.floor(max_flow_val / 2) + target_team_games_left
             if allow_ties
@@ -101,27 +97,76 @@ def get_possibility(target_team, pt_filepath, schedule_filepath, allow_ties):
         max_flow_path,
     )
 
+def get_max_flow(G, source, sink, teams, teams_data, target_max_flow, top_n=1):
+
+    if top_n <= 1:
+        # Check for negative edge weights
+        if any(edge[2]["capacity"] < 0 for edge in G.edges(teams, data=True)):
+            return False, float('-inf'), None
+
+        max_flow_val, max_flow_path = nx.maximum_flow(G, source, sink)
+        return max_flow_val == target_max_flow, max_flow_val, max_flow_path
+
+    else:
+        # Track best result
+        max_max_flow, max_max_path = float('-inf'), None
+
+        # Loop through every combo of teams to ignore
+        for team_combo in combinations(teams, top_n - 1):
+
+            # Set ignored edge weights
+            for team in team_combo:
+                G[team][sink]['capacity'] = 100
+
+            try:
+                # Check for negative edge weights
+                if any(edge[2]['capacity'] < 0 for edge in G.edges(teams, data=True)):
+                    continue
+
+                max_flow_val, max_flow_path = nx.maximum_flow(G, source, sink)
+
+                # Check if valid flow
+                if max_flow_val == target_max_flow:
+                    return True, max_flow_val, max_flow_path
+
+                # Track best result
+                if max_flow_val > max_max_flow:
+                    max_max_flow = max_flow_val
+                    max_max_path = max_flow_path
+            
+            # Restore Edge Weights
+            finally:
+                for team in team_combo:    
+                    G[team][sink]["capacity"] = teams_data[team]
+
+        return False, max_max_flow, max_max_path
+
+
 def main():
-    target_team = "Delhi Capitals"
+    target_team = "Punjab Kings"
+    top_n = 4
+    allow_ties = False
+    success, scheduled_count, total_matches, max_flow_path = (
+        get_possibility(
+            target_team,
+            "data/ipl_2024_points_table_edit.csv",
+            "data/ipl_2024_schedule_edit.csv",
+            allow_ties=allow_ties,
+            top_n=top_n,
+        )
+    )
     # simple_check, success, scheduled_count, total_matches, max_flow_path = (
     #     get_possibility(
     #         target_team,
-    #         "data/ipl_2024_points_table_edit.csv",
-    #         "data/ipl_2024_schedule_edit.csv",
-    #         allow_ties=True,
+    #         "data/ipl_2025_points_table.csv",
+    #         "data/ipl_2025_schedule.csv",
+    #         allow_ties=allow_ties,
+    #         top_n=top_n,
     #     )
     # )
-    simple_check, success, scheduled_count, total_matches, max_flow_path = (
-        get_possibility(
-            target_team,
-            "data/ipl_2025_points_table.csv",
-            "data/ipl_2025_schedule.csv",
-            allow_ties=True,
-        )
-    )
-    if simple_check:
-        print(f'{target_team} can end at the top: {success}')
-        print(f'Scheduled {scheduled_count} out of {total_matches} matches.')
+
+    print(f'{target_team} can end in the top {top_n}: {success}')
+    print(f'Scheduled {scheduled_count} out of {total_matches} matches.')
 
 
 if __name__ == '__main__':
